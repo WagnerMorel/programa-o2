dd2cb30381e72429c29a72a20fbb21a506849897
aluno <aluno@MSCGRESL05014.estacio.acad>
2017-08-08 21:46:45 -0400

    mudança de pacote

---
+++ src/Teste/Cliente.java
@@ -0,0 +1,60 @@
+package Teste;
+
+
+public class Cliente {
+    private String nome;
+    private String email;
+    private String senha;
+    private char sexo;
+    private String leitura;
+    private String timeFutebol;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getLeitura() {
+        return leitura;
+    }
+
+    public void setLeitura(String leitura) {
+        this.leitura = leitura;
+    }
+
+    public String getTimeFutebol() {
+        return timeFutebol;
+    }
+
+    public void setTimeFutebol(String timeFutebol) {
+        this.timeFutebol = timeFutebol;
+    }
+    
+}
